efa3f0551e714dd9234b0fb7a6e3ed151b425e2f
Alex Mwangi <alex@stl-horizon.com>
2022-04-20 10:01:04 +0300

    Made Login to work - added route to action property - Added err msg

--- resources/views/Admin/admin_login.blade.php
+++ resources/views/Admin/admin_login.blade.php
@@ -38,18 +38,22 @@
                             <div class="logo">
                                 <img src="{{asset('panel/assets/images/logo.png')}}" alt="" class="img-fluid">
                             </div>
-                            <form action=" " class="d-block" method="post">
-                                
+                            @if(Session::has('error'))
+                              <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                                 {{Session::get('error')}}
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>
+                            @endif
+                            <form action="{{route('admin.login')}} " class="d-block" method="post">
+                                @csrf
                                 <div class="form-group icon_parent">
-                                     <label for="password">Email</label>
-         <input id="email" type="email" class="form-control"  name="email" value=" " required autocomplete="email" autofocus placeholder="Email Address">
-              <span class="icon_soon_bottom_right"><i class="fas fa-envelope"></i></span>
-                                 
+                                     <label for="email">Email</label>
+                                         <input id="email" type="email" class="form-control"  name="email" placeholder="Email Address">
+                                         <span class="icon_soon_bottom_right"><i class="fas fa-envelope"></i></span>                                 
                                 </div>
                                 <div class="form-group icon_parent">
                                     <label for="password">Password</label>
-       <input id="password" type="password" class="form-control"   name="password" required autocomplete="current-password" placeholder="Password">
-                                        
+                                     <input name="password" type="password" class="form-control"   name="password" placeholder="Password">                                        
                                     <span class="icon_soon_bottom_right"><i class="fas fa-unlock"></i></span>
                                 </div>
                                 <div class="form-group">
