dc9fe7790b514a29b660ed5adf5533a0b9bd39ff
Alex Mwangi <alex@stl-horizon.com>
2022-04-20 10:01:49 +0300

    Added authenticate logic

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,10 +4,29 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
-    public function Login()
+    public function Login(Request $request)
+    {
+
+        $loginCreds = $request->all();
+
+        if (Auth::guard('admin')->attempt([
+
+            'email' => $loginCreds['email'],
+            'password' => $loginCreds['password'],
+
+        ])) {
+
+            return redirect()->route('admin.dashboard');
+        } else {
+
+            return back()->with('error', 'Invalid Email / Password');
+        }
+    }
+    public function Index()
     {
 
         return view('Admin.admin_login');
